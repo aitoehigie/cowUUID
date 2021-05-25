3597dc3506ad2999a9803e8aeeb4a7cb492ebde7
ehigie aito (Pystar) <aitoehigie@gmail.com>
2021-05-25 03:47:53 +0100

    models and pre-commits

---
+++ models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.sql import func
+
+from utils.database import Base
+
+
+class UUID(Base):
+    __tablename__ = "uuids"
+    id = Column(name="id", type_=Integer, primary_key=True, unique=True, index=True)
+    uuid = Column(name="uuid", type_=String, index=True)
+    timestamp = Column(
+        name="timestamp", type=DateTime(timezone=True), server_default=func.now()
+    )
